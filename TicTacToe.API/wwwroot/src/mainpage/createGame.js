8ccc96687e361b230a140b4e6436f8e2184d8481
Arinovich Vlad <vlad23102@gmail.com>
2021-04-15 10:19:23 +0300

    fix length of inputs

--- TicTacToe.API/wwwroot/src/mainpage/createGame.js
+++ TicTacToe.API/wwwroot/src/mainpage/createGame.js
@@ -1,20 +1,25 @@
 ﻿$(document).ready(function () {
 	$("#startGame").click(function () {
-		var Name = $("#gameName").val();
-		var Tags = $("#gameTags").val();
-		console.log({ Name }, { Tags });
-		let res = $.ajax({
-			type: "POST",
-			contentType: "application/json; charset=utf-8",
-			dataType: "json",
-			url: window.location.origin + "/api/game/createGame",
-			data: JSON.stringify({ Name: Name, Tags: Tags }),
-			success: function (gameId) {
-				console.log(gameId.id);
-				localStorage.setItem("gameId", gameId.id);
-				localStorage.setItem("move", "X");
-				window.open("game.html", "_self");
-			},
-		});
+		let Name = $("#gameName").val();
+		let Tags = $("#gameTags").val();
+		if(Name.length>=3 && Tags.length>= 3) {
+			let res = $.ajax({
+				type: "POST",
+				contentType: "application/json; charset=utf-8",
+				dataType: "json",
+				url: window.location.origin + "/api/game/createGame",
+				data: JSON.stringify({Name: Name, Tags: Tags}),
+				success: function (gameId) {
+					console.log(gameId.id);
+					localStorage.setItem("gameId", gameId.id);
+					localStorage.setItem("move", "X");
+					window.open("game.html", "_self");
+				},
+			});
+		}
+		else 
+		{
+			alert("Minimum length of the Name and Tags is 3 character!!!")
+		}
 	});
 });
